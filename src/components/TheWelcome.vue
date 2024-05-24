fa3570a92e42af830ac9ead74b071bf37686d0f4
Marcley <marcleyrosa@gmail.com>
2024-05-23 22:28:08 -0300

    feat: componentes iniciais + NavBar

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <p>Test</p>
+  </WelcomeItem>
+
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+  </WelcomeItem>
+</template>
