6dacacc2b80837a92ed262e2c85c2cdb0063cc84
Marcley <marcleyrosa@gmail.com>
2024-05-23 22:29:01 -0300

    feat: gitignore e componente home

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue';
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <NavBar v-once />
+    <TheWelcome />
+  </main>
+</template>
