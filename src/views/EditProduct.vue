1bdf7963e6593d255c317a716f1c9a65caedf75f
Marcley <marcleyrosa@gmail.com>
2024-06-02 21:07:06 -0300

    feat: import function fetch

--- src/views/EditProduct.vue
+++ src/views/EditProduct.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-import ProductNew from '@/components/ProductNew.vue';
-import { useFetchUpdate } from '@/utils/fetch';
-import { ref } from 'vue';
-import { useRoute } from 'vue-router';
+import ProductNew from '@/components/ProductNew.vue'
+import { useFetchUpdate } from '@/utils/Functionsfetch'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 
 const route = useRoute()
 
 const url = ref(`http://localhost:3000/products/${route.params.id}`)
-
 </script>
 
 <template>
