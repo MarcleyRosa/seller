81c990987bfd9dd67766f5d1de3be44838893d08
Marcley <marcleyrosa@gmail.com>
2024-06-02 21:09:53 -0300

    feat: interface para tipagem

--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-import { useFetchGet } from '@/utils/fetch';
-import { onMounted, ref } from 'vue';
-import router from '@/router';
-import { useRoute } from 'vue-router';
+import { useFetchGet } from '@/utils/Functionsfetch'
+import { onMounted, ref } from 'vue'
+import router from '@/router'
+import { useRoute } from 'vue-router'
 
 interface store {
-  store_id: string, id: string, title: string, price: string, image: string
+  store_id: string
+  id: string
+  title: string
+  price: string
+  image: string
 }
 
 const data = ref<store[]>([])
 const route = useRoute()
 
-const edit = async ({ target: { id }}: any) => {
+const edit = async ({ target: { id } }: any) => {
   router.push(`/products/${id}/store/${route.params.id}`)
 }
 
@@ -22,7 +26,6 @@ const create = () => {
 onMounted(async () => {
   data.value = await useFetchGet(`http://localhost:3000/store/${route.params.id}/products`)
 })
-
 </script>
 
 <template>
@@ -30,16 +33,17 @@ onMounted(async () => {
     <h1>Produtos</h1>
     <button @click="create">Criar Produto</button>
     <div v-for="{ title, id, price, image } in data" :key="id">
-      <span>{{ title }}</span> <br>
+      <span>{{ title }}</span> <br />
       <p>{{ price }}</p>
       <button>
-        <img v-if="image" width="100px" :src="image" alt="">
-        <img v-else width="100px" src="../assets/produto-sem-foto.png" alt="">
-      </button> <br> <br>
-      <button @click="edit" :id=id>Editar</button>
-      <br><br>
-      <hr>
+        <img v-if="image" width="100px" :src="image" alt="" />
+        <img v-else width="100px" src="../assets/produto-sem-foto.png" alt="" />
+      </button>
+      <br />
+      <br />
+      <button @click="edit" :id="id">Editar</button>
+      <br /><br />
+      <hr />
     </div>
-
-    </div>
+  </div>
 </template>
