b6af28a43e0a0ee158bceeed3326d2c5b40f964b
Marcley <marcleyrosa@gmail.com>
2024-06-06 15:17:46 -0300

    feat: componentizado para criar e atualizar produtos inclusive com imagem

--- src/components/ProductNew.vue
+++ src/components/ProductNew.vue
@@ -1,42 +1,52 @@
 <script setup lang="ts">
-import router from '@/router';
-import { reactive } from 'vue'
-import { useRoute } from 'vue-router';
+import { Request } from '@/utils/fetch'
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
-const title = defineModel('title', { default: ''})
-const price = defineModel('price', { default: ''})
-const img = defineModel('img', { default: ''})
-
-const route = useRoute()
-
-interface iBody {
-  product: { title: string, price: string, image: string }
-}
-
-const request = defineProps<{
-  useFetch: (url: string, body: iBody) => {}
-  url: string
+const props = defineProps<{
+  method: 'post' | 'patch'
+  path: string
 }>()
 
-const save = async () => {
-  
-  const body = reactive({ product: { store_id: route.params.store_id, title: title.value, price: price.value, image: img.value } })
-  request.useFetch(request.url, body)
- 
-  router.back()
+const createImage = (event: Event) => {
+  const input = event.target as HTMLInputElement
+  if (input.files && input.files.length > 0) {
+    image.value = input.files[0]
+  }
 }
 
+const title = defineModel('title', { default: '' })
+const price = defineModel('price', { default: '' })
+const image = ref<File | null>(null)
+
+const route = useRoute()
+const router = useRouter()
+const request = new Request('http://localhost:3000')
+
+const save = async () => {
+  const formData = new FormData()
+
+  formData.append('product[store_id]', route.params.store_id as string)
+  formData.append('product[title]', title.value)
+  formData.append('product[price]', price.value)
+  formData.append('product[image]', image.value as File)
+  await request[props.method](props.path, formData)
+
+  router.back()
+}
 </script>
 
 <template>
   <div>
-    <label for="">Titulo</label> <br>
-    <input v-model="title" type="text">  <br> <br>
-    <label for="">Preço</label> <br>
-    <input v-model="price" type="text"> <br> <br>
-    <label for="">Imagem</label> <br>
-    <input v-model="img" type="text"> <br> <br> <br>
-
+    <label for="">Titulo</label> <br />
+    <input v-model="title" type="text" /> <br />
+    <br />
+    <label for="">Preço</label> <br />
+    <input v-model="price" type="text" /> <br />
+    <br />
+    <label for="">Imagem</label> <br />
+    <input @change="createImage" type="file" /> <br />
+    <br />
 
     <button @click="save">Salvar</button>
   </div>
