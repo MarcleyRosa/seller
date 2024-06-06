1aecce6037a4a2f8da4bf648e3dc9f1a885396cc
Marcley <marcleyrosa@gmail.com>
2024-06-06 15:20:32 -0300

    feat: componente para configurar edição de produto

--- src/views/EditProduct.vue
+++ src/views/EditProduct.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import ProductNew from '@/components/ProductNew.vue'
-import { useFetchUpdate } from '@/utils/Functionsfetch'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 
-const url = ref(`http://localhost:3000/products/${route.params.id}`)
+const path = ref(`/products/${route.params.id}`)
 </script>
 
 <template>
   <div>
-    <ProductNew :use-fetch="useFetchUpdate" :url="url" />
+    <ProductNew method="patch" :path="path" />
   </div>
 </template>
