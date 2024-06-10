55b22f5685a104ddbcb5cd57d63db2a19db27433
Marcley <marcleyrosa@gmail.com>
2024-06-10 09:18:45 -0300

    feat: component para detalhes do produto

---
+++ src/components/OrderDetails.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface Order {
+  id: string
+  created_at: string
+}
+defineProps<{
+  order: Order
+}>()
+</script>
+<template>
+  <div>
+    <div>
+      <span>{{ order.id }}</span> <br />
+      <p>{{ order.created_at }}</p>
+      <br />
+      <br />
+      <button>Aceitar</button>
+      <button>Recusar</button>
+      <br /><br />
+      <hr />
+    </div>
+  </div>
+</template>
