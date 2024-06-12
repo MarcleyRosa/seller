1fa96dcaf46868f92c1f204ee5153098eeb60a47
Marcley <marcleyrosa@gmail.com>
2024-06-12 10:07:19 -0300

    fix: trocando para useRouter

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import router from '@/router';
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const logout = () => {
   localStorage.removeItem('token')
   router.push('/')
 }
-
 </script>
 
 <template>
