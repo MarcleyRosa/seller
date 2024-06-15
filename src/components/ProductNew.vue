a2c322bc24be41e958720bd5af74c4856486b903
Marcley <marcleyrosa@gmail.com>
2024-06-15 01:48:26 -0300

    feat: fix: adicionando estoque para edição e criação de produto

--- src/components/ProductNew.vue
+++ src/components/ProductNew.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { Request } from '@/utils/fetch'
-import { ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
+import { onMounted, reactive, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { formatCurrency } from '../utils/index'
+import type { Product } from '../utils/interfaces'
 
 const props = defineProps<{
   method: 'post' | 'patch'
@@ -17,32 +19,51 @@ const createImage = (event: Event) => {
 
 const title = defineModel('title', { default: '' })
 const price = defineModel('price', { default: '' })
-const image = ref<File | null>(null)
+const stock = defineModel('stock', { default: 0 })
 
+const image = ref<File | null>(null)
+const url = 'http://localhost:3000'
 const route = useRoute()
-const router = useRouter()
-const request = new Request('http://localhost:3000')
+const request = new Request(url)
+const data = ref<Product>({} as Product)
+
+onMounted(async () => {
+  data.value = await request.get(`/products/${route.params.id}`)
+})
+
+const itens = reactive({
+  store_id: route.params.store_id as string,
+  title: title,
+  price: price,
+  stock: stock,
+  image: image
+})
 
 const save = async () => {
   const formData = new FormData()
+  const body = Object.entries(itens)
 
-  formData.append('product[store_id]', route.params.store_id as string)
-  formData.append('product[title]', title.value)
-  formData.append('product[price]', price.value)
-  formData.append('product[image]', image.value as File)
-  await request[props.method](props.path, formData)
-
-  router.back()
+  body.forEach(([key, value]) => {
+    if (value) formData.append(`product[${key}]`, value as any)
+  })
+  data.value = await request[props.method](props.path, formData)
 }
 </script>
 
 <template>
+  <p>{{ `Titulo: ${data.title}` }}</p>
+  <p>{{ `Preço: ${formatCurrency(+data.price)}` }}</p>
+  <p>{{ `Estoque: ${data.stock}` }}</p>
+  <img :src="url + data.image_url" alt="" />
   <div>
     <label for="">Titulo</label> <br />
     <input v-model="title" type="text" /> <br />
     <br />
     <label for="">Preço</label> <br />
     <input v-model="price" type="text" /> <br />
+    <br />
+    <label for="">Estoque</label> <br />
+    <input v-model="stock" type="number" /> <br />
     <br />
     <label for="">Imagem</label> <br />
     <input @change="createImage" type="file" /> <br />
